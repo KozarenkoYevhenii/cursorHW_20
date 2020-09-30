315285d68a528c3d3c61d2860a77c9566d94553b
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-09-30 17:05:29 +0300

    Updates

---
+++ precache-manifest.4f38794e5c3f0eb9139672abba9511c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3744762f6eb76daf4f610ca21e21e6e0",
+    "url": "/cursorHW_20/index.html"
+  },
+  {
+    "revision": "d2a7bddb0ec66a493693",
+    "url": "/cursorHW_20/static/css/main.d3fe79c7.chunk.css"
+  },
+  {
+    "revision": "8c5e2ea6e86d416d50dc",
+    "url": "/cursorHW_20/static/js/2.24e48600.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursorHW_20/static/js/2.24e48600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a7bddb0ec66a493693",
+    "url": "/cursorHW_20/static/js/main.67789580.chunk.js"
+  },
+  {
+    "revision": "3563cbde3d757d58ea43",
+    "url": "/cursorHW_20/static/js/runtime-main.b7c1d4f6.js"
+  }
+]);
